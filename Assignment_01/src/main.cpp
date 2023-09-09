424e66c5acc9ba163dcd654a5d0a7c26a52c9bc6
Simon Gschnell <simongschnell@gmail.com>
2023-09-09 11:20:22 +0200

    prints io23m005 to the serial monitor if the boot button was pressed

--- Assignment_01/src/main.cpp
+++ Assignment_01/src/main.cpp
@@ -1,15 +1,57 @@
 #include <Arduino.h>
 
-// put function declarations here:
-void led_blinking(int, int);
-
-
-
 struct Color{
 int red;
 int green;
 int blue;
 };
+
+//next color for RGB_BUILTIN function
+void next(Color *);
+//global variables
+Color led = {RGB_BRIGHTNESS,0,0};
+Color * led_ptr = &led;
+
+void setup() {
+  pinMode(RGB_BUILTIN, OUTPUT);
+  pinMode(35, INPUT);
+  Serial.begin(115200);
+  delay(500);
+  neopixelWrite(RGB_BUILTIN, led_ptr->red, led_ptr->green, led_ptr->blue); 
+}
+
+int currentState;
+int lastState= HIGH;
+int alternativeState = HIGH;
+int captureTime;
+
+
+void loop() {
+  //The ESP32-S3 has a 45k ohm internal pull-up/pull-down resistor at GPIO0 
+  //https://docs.espressif.com/projects/esptool/en/latest/esp32s3/advanced-topics/boot-mode-selection.html
+  currentState =digitalRead(35);
+
+  if (lastState == LOW && currentState == HIGH){
+    //added little delay for debounce of button press
+    delay(100);
+    Serial.println("io23m005");
+  }
+  lastState = currentState;
+
+  
+  if(Serial.available()>0){
+    String input = Serial.readStringUntil('\n');
+    //both n and N switch the RGB_BUILTIN color
+    if (input == "n" || input == "N" ) {
+      next(led_ptr);
+      neopixelWrite(RGB_BUILTIN, led_ptr->red, led_ptr->green, led_ptr->blue);
+    }else{
+      Serial.println(input);
+    }
+    
+  }
+  
+}
 
 void next(Color *c){
    if(c->red == RGB_BRIGHTNESS){
@@ -22,84 +64,4 @@ void next(Color *c){
     c->blue = 0;
     c->red = RGB_BRIGHTNESS;
    }
-}
-
-Color led = {RGB_BRIGHTNESS,0,0};
-
-void setup() {
-  pinMode(RGB_BUILTIN, OUTPUT);
-  pinMode(35, INPUT);
-  Serial.begin(115200);
-  delay(500);
-  Color * led_ptr = &led;
-  neopixelWrite(RGB_BUILTIN, led_ptr->red, led_ptr->green, led_ptr->blue);
-  
-  
-   
-}
-
-int currentState;
-int lastState= HIGH;
-int stateChange =0;
-int startTime= 0;
-void loop() {
-  /*
-    At start, the RGB-LED is red. When receiving ”n” via the serial port, switch the LED to green. When receiving
-    “n” again, switch it to blue. Receiving another “n” will start the cycle from the beginning (red > green > blue >
-    red > green > …).
-  */
-  Color * led_ptr = &led;
-  currentState =digitalRead(35);
-  
-  if (currentState != stateChange){
-    startTime = millis();
-    stateChange = currentState;
-  }
-
-  if (millis() - startTime > 50){
-  if(lastState == HIGH && currentState == HIGH){
-    Serial.println("The state changed from LOW to HIGH");
-}
-  // save the last state
-  lastState = currentState;
-  }
-  /* if (currentState != LOW){
-    startTime = millis();
-  }
-
-  if(millis() - startTime > 50){
-    
-    if (lastState == HIGH && currentState == LOW){
-    Serial.println("io23m005");
-    
-  }
-    lastState=currentState;
-  } */
-  
-  
-  
-  
-  
-  if(Serial.available()>0){
-    
-    
-    String input = Serial.readStringUntil('\r\n');
-    if (input == "n") {
-      next(led_ptr);
-      
-      neopixelWrite(RGB_BUILTIN, led_ptr->red, led_ptr->green, led_ptr->blue);
-    }else{
-      Serial.println(input);
-    }
-    
-    
-  }
-  
-}
-
-void led_blinking(int LED_PIN, int DELAY) {
-  digitalWrite(LED_PIN, HIGH);
-  delay(DELAY);
-  digitalWrite(LED_PIN, LOW);
-  delay(DELAY);
 }
